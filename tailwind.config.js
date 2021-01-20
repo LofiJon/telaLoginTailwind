76b9ad4cbce8dc64e2b7cce0817fcbe34d1604a1
Jonathan Costa <61572619+jonathanmalagueta@users.noreply.github.com>
2021-01-20 17:33:51 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+    extend: {
+      'azulau' : '##87CEFA'
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
